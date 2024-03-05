d243dd1217aae3cf4c96de0582264e5b5f869cfc
Simen Mailund Svendsen <simen@erus.no>
2024-03-05 16:53:55 +0100

    which key bindings

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,12 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+--
+local wk = require("which-key")
+
+wk.register({
+  o = {
+    name = "obsidian", -- optional group name
+    s = { "<cmd>ObsidianSearch<cr>", "search" }, -- create a binding with label
+  },
+}, { prefix = "<leader>" })
