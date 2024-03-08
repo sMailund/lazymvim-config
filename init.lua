7727f2fbd606a0b63e3055ed2fe51a98cb0d3e91
Simen Mailund Svendsen <simen@erus.no>
2024-03-08 15:34:17 +0100

    dap setup

--- init.lua
+++ init.lua
@@ -1,2 +1,21 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
+
+local dap = require("dap")
+
+dap.adapters.coreclr = {
+  type = "executable",
+  command = "/home/mailund/bin/netcoredbg/netcoredbg",
+  args = { "--interpreter=vscode" },
+}
+
+dap.configurations.cs = {
+  {
+    type = "coreclr",
+    name = "launch - netcoredbg",
+    request = "launch",
+    program = function()
+      return vim.fn.input("Path to dll", vim.fn.getcwd() .. "/bin/Debug/", "file")
+    end,
+  },
+}
